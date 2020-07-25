a18fab153e74cc6248a34ae4ebc39a7eb6b3cbbe
rafaasimi <rafaasimi@hotmail.com>
2020-07-25 18:17:42 -0300

    add particles, buttons and changed colors

--- js/index.js
+++ js/index.js
@@ -1,15 +1,14 @@
-const root = document.querySelector('#root');
-const textTitle = 'Dificuldade';
-
+const root = document.querySelector('#root')
+const textTitle = 'Dificuldade'
 
 const title = Title`
-    color: #F0F0F0;
+    color: #F0F0F0; 
     font-size: 2.5rem;
-    letter-spacing: 1.5px;
+    letter-spacing: 1.2px;
     margin-bottom: 4rem;
     ${textTitle}
 `
 
-
-root.insertAdjacentHTML('beforeend', title);
-root.insertAdjacentHTML('beforeend', wrapperCharacters);
+root.insertAdjacentHTML('beforeend', title)
+root.insertAdjacentHTML('beforeend', wrapperCharacters)
+root.insertAdjacentHTML('beforeend', slide)
--- js/styles.js
+++ js/styles.js
@@ -1,30 +1,51 @@
-const style = `<style>
-        * {
-            margin: 0;
-            padding: 0;
-            border: none;
-        }
+const css = `
+<style>
+    :root {
+        --line-height: 10px;
+        --happy-color: #f5f6fa;
+        --action-color: #b33939;
+    }
 
-        body {
-            background-color: #2f3640;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
+    * {
+        margin: 0;
+        padding: 0;
+        border: none;
+    }
 
-        #root {
-            box-sizing: border-box;
-            text-align: center;
-            padding-top: 4rem;
-            background-color: #1e272e;
-            width: 40vw;
-            min-width: 768px;
-            height: 65vh;
-            font-family: 'Bungee', cursive;
-            border-radius: 4px;
-            box-shadow: 2px 2px 15px rgba(0,0,0,0.1);
-        }
-    </style>`
+    body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        background-color: #2f3640;
+    }
 
-const head = document.querySelector('head').insertAdjacentHTML('beforeend', style);
+    #particles-js {
+        position: absolute;
+        width: 100%;
+        height: 100vh;
+        z-index: 1;
+    }
+
+    #root {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        box-sizing: border-box;
+        font-family: 'Bungee', cursive;
+        background-color: #1e272e;
+        width: 40vw;
+        min-width: 768px;
+        height: 65vh;
+        text-align: center;
+        padding-top: 4rem;
+        padding-bottom: 8rem;
+        border-radius: 4px;
+        box-shadow: 2px 2px 15px rgba(0,0,0,0.1);
+        z-index: 10;
+    }
+</style>`
+
+const head = document.querySelector('head')
+
+head.insertAdjacentHTML('beforeend', css)
